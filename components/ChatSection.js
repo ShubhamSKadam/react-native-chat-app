4e44c1576130081aee982847ee60b9615d50f79d
Shubham.kadam <shubham.kadam@Shubhams-Mac.local>
2023-08-02 15:03:09 +0530

    update-2 Chat section partial completed

---
+++ components/ChatSection.js
@@ -0,0 +1,53 @@
+import { FlatList, StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+
+function renderChatData({ item }) {
+  return (
+    //container
+    <View style={styles.container}>
+      {/* Profile Image */}
+      <View>
+        <Image source={item.profileImage} style={styles.imageProfile} />
+      </View>
+
+      {/* Name and recent chat */}
+      <View>
+        <View>
+          <Text style={styles.usernameText}>{item.username}</Text>
+        </View>
+
+        <View style={styles.chatHistoryContainer}>
+          <Text style={styles.grayText}>{item.chatHistory[0].sent}</Text>
+          <Text style={styles.grayText}>{item.timeStamp}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+const ChatSection = ({ dummyData }) => {
+  return <FlatList data={dummyData} renderItem={renderChatData} />;
+};
+
+export default ChatSection;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 14,
+  },
+  imageProfile: {
+    height: 70,
+    width: 70,
+    borderRadius: 35,
+  },
+  usernameText: {
+    fontSize: 18,
+  },
+  chatHistoryContainer: {
+    flexDirection: "row",
+  },
+  grayText: {
+    color: "gray",
+  },
+});
--- components/FavoritesSection.js
+++ components/FavoritesSection.js
@@ -1,39 +1,6 @@
 import { FlatList, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
-const dummyData = [
-  {
-    id: 1,
-    username: "Amlan",
-    profileImage: require("../assets/Images/profilePics/batman.jpg"),
-  },
-  {
-    id: 2,
-    username: "Aditya",
-    profileImage: require("../assets/Images/profilePics/ironMan.jpeg"),
-  },
-  {
-    id: 3,
-    username: "Amlan",
-    profileImage: require("../assets/Images/profilePics/batman.jpg"),
-  },
-  {
-    id: 4,
-    username: "Aditya",
-    profileImage: require("../assets/Images/profilePics/ironMan.jpeg"),
-  },
-  {
-    id: 5,
-    username: "Amlan",
-    profileImage: require("../assets/Images/profilePics/batman.jpg"),
-  },
-  {
-    id: 6,
-    username: "Aditya",
-    profileImage: require("../assets/Images/profilePics/ironMan.jpeg"),
-  },
-];
-
 function renderFavProfiles({ item }) {
   return (
     <View style={styles.container}>
@@ -43,7 +10,7 @@ function renderFavProfiles({ item }) {
   );
 }
 
-const FavoritesSection = () => {
+const FavoritesSection = ({ dummyData }) => {
   return (
     <FlatList
       data={dummyData}
