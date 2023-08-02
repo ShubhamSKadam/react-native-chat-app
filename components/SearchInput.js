9c736c1f49759ce689ae61e2293768bb2ad4a9e3
Shubham.kadam <shubham.kadam@Shubhams-Mac.local>
2023-08-02 12:28:04 +0530

    Initialized project

---
+++ components/SearchInput.js
@@ -0,0 +1,16 @@
+import { View, Text, TextInput , StyleSheet} from "react-native";
+import React from "react";
+
+const SearchInput = () => {
+  return (
+    <View>
+      <TextInput placeholder="Search" />
+    </View>
+  );
+};
+
+export default SearchInput;
+
+const styles = StyleSheet.create({
+    
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import SearchInput from "../components/SearchInput";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <SearchInput />
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
