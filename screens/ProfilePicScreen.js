6a814ab3bc50339c3a59f43fb39be74954e4994f
Shubham.kadam <shubham.kadam@Shubhams-Mac.local>
2023-08-04 11:59:44 +0530

    Profile name added

--- screens/ProfilePicScreen.js
+++ screens/ProfilePicScreen.js
@@ -1,14 +1,22 @@
 import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
 const ProfilePicScreen = () => {
+  const navigation = useNavigation();
+
   const user = useSelector((state) => state.Chats.selectedUser);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: user.username,
+    });
+  });
 
   const userImage = user.profileImage;
   console.log(userImage);
   return (
-    <View>
+    <View style={styles.imageContainer}>
       <Image source={userImage} style={styles.image} />
     </View>
   );
@@ -17,8 +25,14 @@ const ProfilePicScreen = () => {
 export default ProfilePicScreen;
 
 const styles = StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   image: {
-    height: 200,
-    width: 200,
+    height: 300,
+    width: 300,
+    borderRadius: 150,
   },
 });
